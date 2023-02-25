e5b670e12277459a8aeb94526163949e336afde4
cyntia-seumo-rocketcyber <jseumo@rocketcyber.com>
2023-02-24 23:34:16 -0600

    Add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,45 @@
+import {useState} from 'react'
+import Link from 'next/link'
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(true)
+  const menuItems = [{
+    name: 'Home', url: '/'
+  }, {
+    name: 'Shop', url: '/shop'
+  }, {
+    name: 'All Posts', url: '/posts'
+  }, {
+    name: 'About Me', url: '/about'
+  }, {
+    name: 'Salvation', url: '/salvation'
+  }, {
+    name: 'Instagram', url: 'https://www.instagram.com/overcomewithchrist/'
+  }, {
+    name: 'Youtube', url: 'https://www.youtube.com/@overcomewithchrist'
+  }]
+
   return (<>
-      <div> The header</div>
-    </>)
+    {/*Menu toggle button*/}
+    <button type={'button'} onClick={() => setIsOpen(!isOpen)}>
+      <span className="sr-only">Open menu</span>
+      {/*Show hamburger menu if menu isn't open*/}
+      {!isOpen && <svg viewBox={'0 0 24 24'} height="40" width="40">
+        <line x1="5" y1="5" x2="19" y2="5" rx={'2'} stroke={'#FFB284'} strokeWidth={'2'} />
+        <line x1="5" y1="10" x2="19" y2="10" rx={'2'} stroke={'#FFB284'} strokeWidth={'2'} />
+        <line x1="5" y1="15" x2="19" y2="15" rx={'2'} stroke={'#FFB284'} strokeWidth={'2'} />
+      </svg>}
+      {/*Show X if menu is open*/}
+      {isOpen && <svg viewBox={'0 0 24 24'} height="40" width="40">
+        <line x1="5" y1="5" x2="19" y2="19" rx={'2'} stroke={'#FFB284'} strokeWidth={'2'} />
+        <line x1="19" y1="5" x2="5" y2="19" rx={'2'} stroke={'#FFB284'} strokeWidth={'2'} />
+      </svg>}
+    </button>
+    {/*Menu items*/}
+    <div className={`nav-items-container ${isOpen ? 'block openMenu' : 'hidden'}`}>
+      {menuItems?.map((menuItem) => (<Link href={menuItem.url} key={menuItem.name}>
+          {menuItem.name}
+        </Link>))}
+    </div>
+  </>)
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 const NotFound = () => {
   return (
     <>
-      <h1> 404 page not found</h1>
+      <h1> 404 </h1>
+      <p> oops, not sure how you got here but this page doesn&apos;t exist.</p>
+      <Link href={'/'}> Take me to greener pastures </Link>
     </>
   )
 }
