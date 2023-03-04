844c7426a1925753929edd34357b93a7e4b95fd6
Cyntia S <jackycyntias@gmail.com>
2023-03-04 15:06:45 -0600

    center 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,10 +5,10 @@ const NotFound = () => {
   return (
     <Layout pageTitle={''}>
       <div className={'w-full h-96 flex content-center items-center'}>
-        <div className={'p-10 text-center'}>
+        <div className={'p-10 mx-auto text-center'}>
           <p className={'text-4xl font-semibold'}>Four - oh - Four</p>
           <p> oops, not sure how you got here but this page doesn&apos;t exist.</p>
-          <div className={'mt-5 w-full md:w-3/8 bg-primary rounded-2xl px-3 pt-1'}>
+          <div className={'mt-5 w-full md:w-3/8 bg-primary rounded-2xl px-3 pt-1 '}>
             <Link className={'text-white hover:text-black'} href={'/'}> Take me to greener pastures </Link>
           </div>
         </div>
