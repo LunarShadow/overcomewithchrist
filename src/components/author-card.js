ff06ed51db67ccfda20961cc7946011e73b8d2d2
cyntia-seumo-rocketcyber <jseumo@rocketcyber.com>
2023-06-01 20:06:26 -0500

    update author card image link

--- src/components/author-card.js
+++ src/components/author-card.js
@@ -4,7 +4,7 @@ export default function AuthorCard(){
 
   return (<>
     <div className={'relative w-48 h-48 mx-auto mt-5'}>
-      <Image className={'rounded-full'} src={'/img/blog/profile_pic.png'} alt={'author image'} fill />
+      <Image className={'rounded-full'} src={'/img/profile_pic.png'} alt={'author image'} fill />
     </div>
     <div className={'text-center'}>
       <p className={'font-semibold text-black mt-3 text-xl'}>Jacky Cyntia Seumo</p>
--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -4,7 +4,9 @@ const ShopHome = () => {
   return (
     <Layout pageTitle={'Shop'}>
       <h1 className={'pageTitle'}>Shop</h1>
-
+      <div className={'h-96 flex content-center items-center px-4'}>
+        <h1 className={'text-center w-full'}> Shop Coming soon.</h1>
+      </div>
     </Layout>
   )
 }
