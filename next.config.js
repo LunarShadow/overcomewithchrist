f7ba6cc4fa50e4c2553b135a7583fea726d2dc90
cyntia-seumo-rocketcyber <jseumo@rocketcyber.com>
2023-06-05 23:45:29 -0500

    Update TOML

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ const nextConfig = {
       }
     )
     return cfg;
+  },
+  async redirects() {
+    return [{
+      source: 'shop.overcomewithchrist.com', destination: '/shop', permanent: true,
+    }]
   }
 }
 module.exports = withSentryConfig(
